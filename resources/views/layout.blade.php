97b6a225f04e6da8907d1ccef67e9dbd6c729d5c
Artem Kutovoy <kutartal@gmail.com>
2015-10-02 18:54:21 +0300

    delete welcome

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Flyer</title>
+    <link rel="stylesheet" href="/css/app.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+</html>
